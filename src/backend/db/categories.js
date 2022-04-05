c1e4952722bf81e6733dfbc0895d36d3ddcf1523
dipen2000 <dipenchavda2000@gmail.com>
2022-04-05 23:03:07 +0530

    updated categories.js

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,27 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: "All",
+    category: "ALL",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    categoryName: "Sneaker ads",
+    category: "SNEAKER_ADS",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+    categoryName: "Sneaker origins",
+    category: "SNEAKER_ORIGINS",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Sneaker unboxing",
+    category: "SNEAKER_UNBOXING",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Sneaker shopping",
+    category: "SNEAKER_SHOPPING",
   },
 ];
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar flex-row align-center-flex justify-space-between-flex"></nav>
+    </header>
+  );
+};
+
+export { Navbar };
