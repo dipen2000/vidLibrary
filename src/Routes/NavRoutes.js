bd4657d9d9c0bd64fcf7e288a788c8b001b3b079
dipen2000 <dipenchavda2000@gmail.com>
2022-04-06 23:39:48 +0530

    initial skeleton home page done

--- src/Routes/NavRoutes.js
+++ src/Routes/NavRoutes.js
@@ -5,6 +5,8 @@ import { Playlists } from "../pages/Playlists/Playlists";
 import { Liked } from "../pages/Liked/Liked";
 import { WatchLater } from "../pages/WatchLater/WatchLater";
 import { History } from "../pages/History/History";
+import { Logout } from "../pages/Logout/Logout";
+import { Signup } from "../pages/Signup/Signup";
 import Mockman from "mockman-js";
 
 const NavRoutes = () => {
@@ -12,11 +14,13 @@ const NavRoutes = () => {
     <Routes>
       <Route path="/mock" element={<Mockman />} />
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/playlist" element={<Playlists />} />
       <Route path="/liked" element={<Liked />} />
       <Route path="/watch-later" element={<WatchLater />} />
       <Route path="/history" element={<History />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,71 @@
 import "./Login.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Navbar } from "../../components/Navbar/Navbar";
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setError] = useState();
+  const [loginInput, setLoginInput] = useState({
+    input: {},
+    hide: { pwd: true },
+  });
+  const navigate = useNavigate();
+
+  const loginInputHandler = (e) => {
+    const { name, value } = e.target;
+    setLoginInput({
+      ...loginInput,
+      input: { ...loginInput.input, [name]: value },
+    });
+  };
+
+  const guestLogin = {
+    email: "adarshbalika@gmail.com",
+    password: "adarshBalika123",
+  };
+
+  //   console.log(loginInput.input);
+  return (
+    <>
+      <Navbar />
+      <section>
+        <form>
+          <h2>Login</h2>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your e-mail here"
+            value={loginInput.input.email}
+            onChange={loginInputHandler}
+          />
+          <br />
+          <input
+            type={loginInput.hide.pwd ? "password" : "text"}
+            name="password"
+            placeholder="Enter your password"
+            value={loginInput.input.password}
+            onChange={loginInputHandler}
+          />
+          <span
+            className="curs-point"
+            onClick={() =>
+              setLoginInput({
+                ...loginInput,
+                hide: { pwd: !loginInput.hide.pwd },
+              })
+            }
+          >
+            {loginInput.hide.pwd ? "show" : "hide"}
+          </span>
+          <br />
+          <button className="curs-point">login</button>
+          <p>
+            New to SHOETUBE? <Link to="/signup">Create new account</Link>
+          </p>
+          <div className="error-message">{error}</div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export { Login };
